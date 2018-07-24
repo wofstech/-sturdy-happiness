7a3a85e7ddd13c3d9dad3c68b1da4dba10449661
wofstech <gabrielabuka3@gmail.com>
2018-07-24 15:07:33 +0100

    'new payme'

--- paystack/settings.py
+++ paystack/settings.py
@@ -3,7 +3,7 @@ from django.shortcuts import reverse
 PAYSTACK_SECRET_KEY = getattr(settings, "PAYSTACK_SECRET_KEY", None)
 PAYSTACK_PUBLIC_KEY = getattr(settings, 'PAYSTACK_PUBLIC_KEY', None)
 ALLOWED_HOSTS = getattr(settings, 'ALLOWED_HOSTS', [])
-PAYSTACK_WEBHOOK_DOMAIN = getattr(settings, 'PAYSTACK_WEBHOOK_DOMAIN', None)
+PAYSTACK_WEBHOOK_DOMAIN = getattr(settings, 'PAYSTACK_WEBHOOK_DOMAIN', 'www.ywala.com')
 if PAYSTACK_WEBHOOK_DOMAIN:
     ALLOWED_HOSTS.append(PAYSTACK_WEBHOOK_DOMAIN)
 PAYSTACK_FAILED_URL = getattr(
